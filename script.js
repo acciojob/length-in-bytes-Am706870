cc5aaedb3b30fc9652c92c685b3f73ebfbb8e017
Am706870 <88397078+Am706870@users.noreply.github.com>
2024-12-20 13:53:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const byteSize = (str) => {
+	 const blob = new Blob([str]);
+  
+  // Return the size property of the Blob object, which gives the byte size
+	  return blob.size;
+	console.log(byteSize('hello world')); 
+	console.log(byteSize('안녕하세요'));   
+	console.log(byteSize(''));
   // write your code here
 };
-
 // Do not change the code below
 const str = prompt("Enter some string.");
 alert(byteSize(str));
